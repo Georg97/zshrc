387be2e038bd9b4b9a9f4189ab28da96fcfc22ba
Georg97 <75206704+Georg97@users.noreply.github.com>
2023-08-01 08:46:55 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone 
+
+mkdir -p ~/.oh-my-zsh/plugins/copydir && \
+    touch ~/.oh-my-zsh/plugins/copydir/copydir.plugin.zsh && \
+    echo $(curl https://raw.githubusercontent.com/mwilc0x/ohmyzsh/master/plugins/copydir/copydir.plugin.zsh) >> ~/.oh-my-zsh/plugins/copydir/copydir.plugin.zsh
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
